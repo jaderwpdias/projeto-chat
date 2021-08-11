03b67c4879d1bbd8a35e66cf821c1b53c096ee84
Jader Dias <jader.dias@live.com>
2021-08-11 20:26:24 -0300

    primeiro teste desenvolvido com o phpunit ultilizando tdd

--- src/Mensagem/texto.php
+++ src/Mensagem/texto.php
@@ -1,18 +1,28 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace Chatbot\Mensagem;
 
 /**
  * Description of texto
  *
- * @author jaderwillianpereiradias
+ * @author jaderwillianpereiradias <jader.dias@live.com>
  */
 class texto {
-    //put your code here
+    private $id;
+
+    public function __construct(string $id){
+        $this->id = $id;
+    }
+
+    public function mensagem(string $mensagemTexto) :array{
+        return [
+            'recepient' => [
+                'id' => $this->id
+            ],
+            'message' => [
+                'text' => $mensagemTexto,
+                'metadata' => 'DEVELOPER_DEFINED_METADATA'
+            ]
+        ];
+    }
 }
