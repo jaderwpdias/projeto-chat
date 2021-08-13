514dcdc93c7fe813390da502ca3c42e8201fa10f
Jader Dias <jader.dias@live.com>
2021-08-13 13:14:10 -0300

    primeiro commit

--- src/Mensagem/mensagem.php
+++ src/Mensagem/mensagem.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
+
 namespace Chatbot\Mensagem;
 
 interface Mensagem {
 
-	public function __construct(string $id) :void;
-    public function mensagem(string $mensagemTexto) :array;
-
+    public function __construct(string $id): void;
+    public function mensagem(string $mensagemTexto): array;
 }
